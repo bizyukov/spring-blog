85ac5f794510909c2a47edb0ad84998cbdcf8961
Igor Bizyukov <bizykov@gmail.com>
2023-03-07 09:44:31 +0300

    add blog and about pages

---
+++ src/main/java/com/bizyukov/blog/controllers/BlogController.java
@@ -0,0 +1,15 @@
+package com.bizyukov.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        model.addAttribute("title", "Блог");
+        return "blogMain";
+    }
+}
